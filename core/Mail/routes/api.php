341cfb21c0fa40cad194e0a8b68f20a945446b54
AbdlrahmanSaberAbdo <abdlrahmansaber652@gmail.com>
2022-06-19 19:58:33 +0200

    Mail API calls

--- core/Mail/routes/api.php
+++ core/Mail/routes/api.php
@@ -4,7 +4,7 @@ Route::group(['prefix' => 'api', 'middleware' => []], function () {
     # V1
     Route::namespace('Core\Mail\Controllers\API\V1')->prefix('v1')->name('api.v1.')->group(function () {
         #*** Ex: START: Mail ***#
-        // Route::apiResource('mails', 'MailController');
+         Route::apiResource('mails', 'MailController');
         #*** END: Mail ***#
     });
 });
