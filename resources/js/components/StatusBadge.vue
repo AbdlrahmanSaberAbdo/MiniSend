890448c7f24da23d3225fc4cb0fe8dbb922f574f
AbdlrahmanSaberAbdo <abdlrahmansaber652@gmail.com>
2022-06-21 22:25:42 +0200

    StatusBadge component

---
+++ resources/js/components/StatusBadge.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="uppercase text-xs font-bold tracking-widest px-2 py-1 rounded-full" :class="statusClass">
+        {{ status }}
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['status'],
+
+        computed: {
+            statusClass() {
+                if (this.status === 'Sent') {
+                    return 'text-green-900 bg-green-200';
+                }
+
+                if (this.status === 'Posted') {
+                    return 'text-yellow-900 bg-yellow-200';
+                }
+
+                if (this.status === 'Failed') {
+                    return 'text-red-900 bg-red-200';
+                }
+
+                return 'text-gray-900 bg-gray-200';
+            }
+        }
+    }
+</script>
