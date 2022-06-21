ff6a5219023adebc70353029dc55aa1808e9434f
AbdlrahmanSaberAbdo <abdlrahmansaber652@gmail.com>
2022-06-21 22:27:13 +0200

    Add download attachment route

--- core/Mail/routes/api.php
+++ core/Mail/routes/api.php
@@ -4,7 +4,9 @@ Route::group(['prefix' => 'api', 'middleware' => []], function () {
     # V1
     Route::namespace('Core\Mail\Controllers\API\V1')->prefix('v1')->name('api.v1.')->group(function () {
         #*** Ex: START: Mail ***#
-         Route::apiResource('mails', 'MailController');
+         Route::apiResource('emails', 'MailController');
         #*** END: Mail ***#
+
+        Route::get('/attachments/download', 'MailController@download');
     });
 });
