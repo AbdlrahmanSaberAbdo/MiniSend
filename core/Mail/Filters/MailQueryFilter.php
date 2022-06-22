c2bd38a0ab43421f6326ef761d34ec83e3f164e8
AbdlrahmanSaberAbdo <abdlrahmansaber652@gmail.com>
2022-06-22 19:55:33 +0200

    Add filter missing code by wrong

--- core/Mail/Filters/MailQueryFilter.php
+++ core/Mail/Filters/MailQueryFilter.php
@@ -8,6 +8,17 @@ class MailQueryFilter
 {
     public function filter($builder, $value)
     {
+        $regex = '/[a-z]*:[^\s]*@[^\s]*|[a-z]*:"(.*?)"/';
+        $regexResult = null;
+            preg_match_all($regex, $value, $regexResult);
+
+        $data = [];
+
+        // Get only first iteration of regex results
+        foreach ($regexResult[0] as $result) {
+            $aux = explode(':', $result);
+            $data[strtolower($aux[0])] = $aux[1];
+        }
 
         return $builder->where([
             [ 'sender', 'like', isset($data['from']) ? '%' . $data['from'] . '%' : '%' ],
